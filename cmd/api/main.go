0e23bd1ad0fbb7b6f690e7966e6b49b9d6328acc
lucavps <vieira@loomi.com.br>
2026-05-09 09:32:46 -0300

    refactor: add dependecy injection and apply interface segregation

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,16 +2,25 @@ package main
 
 import (
 	"exammple.com/event-booking-api/db"
+	"exammple.com/event-booking-api/internal/handler"
+	"exammple.com/event-booking-api/internal/repository"
 	"exammple.com/event-booking-api/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	db.InitDB()
+	database := db.InitDB()
+
+	eventRepository := repository.NewEventRepository(database)
+	userRepository := repository.NewUserRepository(database)
+
+	userHandler := handler.NewUserHandler(userRepository)
+	eventHandler := handler.NewEventHandler(eventRepository)
+	registrationHandler := handler.NewRegistrationHandler(eventRepository)
 
 	server := gin.Default()
 
-	routes.RegisterRoutes(server)
+	routes.RegisterRoutes(server, userHandler, eventHandler, registrationHandler)
 
 	server.Run(":8080")
 }
